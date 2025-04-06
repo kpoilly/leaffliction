0214a83745cbb24d29a4ac6be3ebb90b7a76c8ef
fmauguin <fmauguin.pro@gmail.com>
2025-04-06 05:02:44 +0200

    add some explanations

--- src/transformation/color_histogram.py
+++ src/transformation/color_histogram.py
@@ -4,9 +4,8 @@ from plantcv import plantcv as pcv
 
 
 def plot_stat_hist(label, scl=1):
-    """
-    Retrieve the histogram x and y values and plot them
-    """
+
+    # outputs observation of the last pcv
     y = pcv.outputs.observations['default_1'][f"{label}_frequencies"]['value']
     x = [
         i * scl
@@ -14,9 +13,13 @@ def plot_stat_hist(label, scl=1):
             f"{label}_frequencies"
         ]['label']
     ]
+    # normalize the hue from 0 - 360 to 0 - 180
     if label == "hue":
         x = x[:int(255 / 2)]
         y = y[:int(255 / 2)]
+
+    # recenter the blue-yellow and green-magenta on the 0-255 scale
+    # CIELAB a* and b* are unsigned integers
     if (
         label == "blue-yellow" or
         label == "green-magenta"
@@ -26,9 +29,6 @@ def plot_stat_hist(label, scl=1):
 
 
 def plot_histogram(image, kept_mask, display_func=None):
-    """
-    Plot the histogram of the image
-    """
 
     # Create a dictionary to store the labels and their corresponding scale
     # some labels are on % so they are multiplied by 2.55
@@ -58,7 +58,7 @@ def plot_histogram(image, kept_mask, display_func=None):
 
     plt.legend()
 
-    plt.title("Color Histogram")
+    plt.title("Color histogram")
     plt.xlabel("Pixel intensity")
     plt.ylabel("Proportion of pixels (%)")
     plt.grid(
